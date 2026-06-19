a7fc4bac736de1606ef443c94822eb9f52559b0b
Kavya3910 <24635kc@hvhs.school.nz>
2026-06-19 15:11:03 +1200

    Variables

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello world!")
+/***** Variables *****/
+let name = "Kavya";
+let pocketMoney = 0;
+let cardNumber = "";
+let cardPin = "";
+let cardBalance = 0;
+let cardExpiryDate = "";
+let cardHolderName = "";
+let productsPrice = "";
+let productsName = "";
+
+/***** Main Code *****/
+console.log(name);
+console.log(pocketMoney);
+
+/***** Functions *****/
