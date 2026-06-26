b5efffb446c7b7d30a855d17b0645659c6b1ec25
Kavya3910 <24635kc@hvhs.school.nz>
2026-06-26 15:13:16 +1200

    Done

--- script.js
+++ script.js
@@ -1,48 +1,20 @@
 /***** Variables *****/
-var name = "Kavya";
+var userName = "";
 var pocketMoney = 0;
-var cardNumber = "";
-var cardPin = "";
-var cardBalance = 0;
-var cardExpiryDate = "";
-var cardHolderName = "";
-var productsPrice = "";
-var productsName = "";
 
 /***** Main Code *****/
-console.log(name);
+console.log(userName);
 console.log(pocketMoney);
 
-const Output = document.getElementById("spaceForJavaScriptOutput");
-const Name_Field = document.getElementById("Name_Field");
-const PocketMoneyField = document.getElementById("PocketMoneyField");
-const CardNumberField = document.getElementById("CardNumberField");
-const CardPinField = document.getElementById("CardPinField");
-const CardBalanceField = document.getElementById("CardBalanceField");
-const CardExpiryDateField = document.getElementById("CardExpiryDateField");
-const CardHolderNameField = document.getElementById("CardHolderNameField");
-const ProductsPriceField = document.getElementById("ProductsPriceField");
-const ProductsNameField = document.getElementById("ProductsNameField");
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const NAME_FEILD = document.getElementById("nameField");
+const POCKET_MONEY_FIELD = document.getElementById("pocketMoneyField");
 
 /***** Functions *****/
-function start() {
-    Output.innerHTML = "Welcome to Kavya's Café! Please enter your details below.";
-  let userName = (Name_Field.value);
-  Output.innerHTML += "User Name: " + userName;
-  let PocketMoney = (PocketMoneyField.value);
-  Output.innerHTML += "Pocket Money: " + PocketMoney;
-  let CardNumber = (CardNumberField.value);
-  Output.innerHTML += "Card Number: " + CardNumber;
-  let CardPin = (CardPinField.value);
-  Output.innerHTML += "Card Pin: " + CardPin;
-  let CardBalance = (CardBalanceField.value);
-  Output.innerHTML += "Card Balance: " + CardBalance;
-  let CardExpiryDate = (CardExpiryDateField.value);
-  Output.innerHTML += "Card Expiry Date: " + CardExpiryDate;
-  let CardHolderName = (CardHolderNameField.value);
-  Output.innerHTML += "Card Holder Name: " + CardHolderName;
-  let ProductsPrice = (ProductsPriceField.value);
-  Output.innerHTML += "Products Price: " + ProductsPrice;
-  let ProductsName = (ProductsNameField.value);
-  Output.innerHTML += "Products Name: " + ProductsName;
+
+function getFormInput(){
+  let userName = (NAME_FEILD.value);
+  OUTPUT.innerHTML = "<p>User Name: " + userName + "</p>";
+  let pocketMoney = (POCKET_MONEY_FIELD.value);
+  OUTPUT.innerHTML += "<p>Pocket Money: " + pocketMoney + "</p>";
 }
